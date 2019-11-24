d1f7573202d333ad08eb87372421efe8c068cb4b
Lauren Cousin <lauren.cousin@gmail.com>
2019-11-23 18:51:41 -0700

    Adding additional doc comments, commenting out WIP game fileio

--- src/minesweeper/GameTimer.java
+++ src/minesweeper/GameTimer.java
@@ -8,6 +8,7 @@ import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
 /**
+ * Represents a timer that keeps track of the current game duration in seconds.
  * 
  * @author laurencousin
  *
@@ -19,6 +20,9 @@ public class GameTimer {
 	
 	private long startTime;
 	
+	/**
+	 * Initializes the timer to be refreshed every 1 second.
+	 */
 	public GameTimer() {
 		timer = new Timer(1000, new ActionListener() {
 			@Override
@@ -28,8 +32,14 @@ public class GameTimer {
 		});
 	}
 	
+	/**
+	 * Creates the timer label, containing a timer icon and displaying the current game 
+	 * duration in seconds.
+	 * 
+	 * @return the timer label
+	 */
 	public JLabel createLblTimer() {
-		// TODO: Margins, font
+		// TODO: Margins, font. Possibly move this to Board
 		lblTimer = new JLabel("");
 		lblTimer.setOpaque(true);
 		lblTimer.setIcon(Board.TIMER_ICON);
@@ -38,16 +48,25 @@ public class GameTimer {
 		return lblTimer;
 	}
 	
+	/**
+	 * Starts the timer after a 1-second delay.
+	 */
 	public void start() {
 		startTime = System.currentTimeMillis();
 		timer.setInitialDelay(1000);
 		timer.start();
 	}
 	
+	/**
+	 * Stops the timer.
+	 */
 	public void stop() {
 		timer.stop();
 	}
 	
+	/**
+	 * Gets the elapsed time and updates the timer label with that time in seconds.
+	 */
 	public void getTime() {
 		long currentTime = System.currentTimeMillis();
 		long elapsedTime = (currentTime - startTime) / 1000;
